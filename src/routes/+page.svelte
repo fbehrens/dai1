bf11658bfe933ff4c9b8936d9b84e4b04ad57fb5
Frank Behrens <fbehrens@gmail.com>
2025-03-07 22:19:00 +0100

    add textarea

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,7 @@
   import { Button } from "$lib/components/ui/button/index.js";
   import ChevronRight from "lucide-svelte/icons/chevron-right";
   import { Input } from "$lib/components/ui/input/index.js";
+  import Textarea from "$lib/components/ui/textarea/textarea.svelte";
 
   const { input, handleSubmit, messages } = useChat();
   $input = "Please summarise dQw4w9WgXcQ";
@@ -17,9 +18,9 @@
     <Message {message} />
   {/each}
   <form onsubmit={handleSubmit}>
-    <Input class="max-w-xs" bind:value={$input} />
-    <Button variant="outline" size="icon" type="submit">
-      <ChevronRight />
-    </Button>
+    <div class="grid w-full gap-2">
+      <Textarea class="max-w" bind:value={$input} />
+      <Button variant="outline" type="submit">send</Button>
+    </div>
   </form>
 </main>
